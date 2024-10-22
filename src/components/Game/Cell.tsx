7a3e28b1714036471f6cd659675afaeaee5124a3
wiibleyde <nathan@bonnell.fr>
2024-10-22 15:59:55 +0200

    Refactoriser le code pour supprimer la variable inutile dans le composant Cell
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/components/Game/Cell.tsx
+++ src/components/Game/Cell.tsx
@@ -4,13 +4,8 @@ export enum CellState {
     Empty
 }
 
-export enum CellWinState {
-    WinnerCell,
-    NotWinnerCell
-}
-
-export function Cell({ state, winnerState }: { state: CellState, winnerState: CellWinState }) {
+export function Cell({ state }: { state: CellState }) {
     return (
-        <div className={"w-16 h-16 rounded-full" + (state === CellState.Empty ? " bg-blue-900" : state === CellState.Player1 ? " bg-red-500" : " bg-yellow-500") + " border-4 " + (winnerState === CellWinState.WinnerCell ? "border-green-500" : "border-gray-200")}></div>
+        <div className={"w-16 h-16 rounded-full" + (state === CellState.Empty ? " bg-blue-900" : state === CellState.Player1 ? " bg-red-500" : " bg-yellow-500")}></div>
     )
 }
