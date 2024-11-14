5a03588e5066305db122ac8e8996bc0af5ddeba8
wiibleyde <nathan@bonnell.fr>
2024-11-14 09:22:10 +0100

    Ajout d'une énumération pour les messages de jeu et refactorisation des hooks pour utiliser GameGridValueInterface
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- server.ts
+++ server.ts
@@ -7,6 +7,13 @@ import { parse } from 'url';
 import next from 'next';
 import { Server } from 'socket.io';
 
+
+enum Messages {
+	PLAYER_ACTION = "player-action",
+	PLAYER_ACTION_RESPONSE = "player-action-response",
+	SEND_PLAYER_TURN = "send-player-turn"
+}
+
 let userCount = 0;
 
 const maxUserCount = 2;
@@ -16,49 +23,49 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+	const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+		const parsedUrl = parse(req.url!, true);
+		handle(req, res, parsedUrl);
+	}).listen(port);
 
-  const io = new Server(server)
+	const io = new Server(server)
 
-  io.on("connection", (socket: Socket) => {
-    // Disconnect new sockets when max connected socket number is reached
-    if (userCount >= maxUserCount) {
-      console.log('Maximum number of user reached:', userCount)
-      socket.disconnect(true) // total disconnection
-      return
-    }
+	io.on("connection", (socket: Socket) => {
+		// Disconnect new sockets when max connected socket number is reached
+		if (userCount >= maxUserCount) {
+			console.log('Maximum number of user reached:', userCount)
+			socket.disconnect(true) // total disconnection
+			return
+		}
 
-    userCount++
-    console.log("Player connected:", socket.id);
-    console.log(`${userCount}/${maxUserCount} users connected`)
-    //TODO check if the good player turn is send for the new connected client (case when a client has left)
-    if (userCount === 1) {
-      socket.emit("send-player-turn", 0)
-    } else {
-      socket.emit("send-player-turn", 1)
-    }
+		userCount++
+		console.log("Player connected:", socket.id);
+		console.log(`${userCount}/${maxUserCount} users connected`)
+		//TODO check if the good player turn is send for the new connected client (case when a client has left)
+		if (userCount === 1) {
+			socket.emit(Messages.SEND_PLAYER_TURN, 0)
+		} else {
+			socket.emit(Messages.SEND_PLAYER_TURN, 1)
+		}
 
-    // Logic for handling player action
-    socket.on("player-action", (gameState, playerTurn) => {
-      console.log(`Player ${socket.id} is making an action, his turn is ${gameState.turn}`);
-      console.log(gameState);
+		// Logic for handling player action
+		socket.on(Messages.PLAYER_ACTION, (gameState, playerTurn) => {
+			console.log(`Player ${socket.id} is making an action, his turn is ${gameState.turn}`);
+			console.log(gameState);
 
-      if (gameState.turn !== playerTurn) {
-        socket.emit("player-action-response", "Non mais oh mec, tu fais quoi la, c'est pas ton tour !")
-      } else {
-        socket.emit("player-action-response", "Tu peux y aller mec")
-        //TODO Call function that will update the gameState then send it to everyone so they can update it
-      }
-    })
+			if (gameState.turn !== playerTurn) {
+				socket.emit(Messages.PLAYER_ACTION_RESPONSE, "Non mais oh mec, tu fais quoi la, c'est pas ton tour !")
+			} else {
+				socket.emit(Messages.PLAYER_ACTION_RESPONSE, "Tu peux y aller mec")
+				//TODO Call function that will update the gameState then send it to everyone so they can update it
+			}
+		})
 
-    socket.on("disconnect", () => {
-      userCount--
-      console.log("Player disconnected:", socket.id);
-    });
-  })
+		socket.on("disconnect", () => {
+			userCount--
+			console.log("Player disconnected:", socket.id);
+		});
+	})
 
-  console.log(`Server listening at http://localhost:${port}`);
+	console.log(`Server listening at http://localhost:${port}`);
 });
