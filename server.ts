9e81f2290b0e39b11410f5124565323150e16c31
Wiibleyde <nathan@bonnell.fr>
2024-12-01 16:07:55 +0100

    refactor: improve code formatting and comments in server.ts
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- server.ts
+++ server.ts
@@ -20,21 +20,21 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  // initializes a custom server to host the websocket server
+	// initializes a custom server to host the websocket server
 	const server = createServer((req: IncomingMessage, res: ServerResponse) => {
 		const parsedUrl = parse(req.url!, true);
 		handle(req, res, parsedUrl);
 	}).listen(port);
 
-  // creates the websocket server
+	// creates the websocket server
 	const io = new Server(server)
 
-  const disconnectAllSockets = () => {
-    Array.from(io.sockets.sockets.values()).forEach((socket) => {
-      console.log(socket.id);
-      socket.disconnect(true);
-    });
-  }
+	const disconnectAllSockets = () => {
+		Array.from(io.sockets.sockets.values()).forEach((socket) => {
+			console.log(socket.id);
+			socket.disconnect(true);
+		});
+	}
 
 	io.on("connection", (socket: Socket) => {
 		// Disconnect new sockets when max connected socket number is reached
@@ -48,7 +48,7 @@ app.prepare().then(() => {
 		console.log("Player connected:", socket.id);
 		console.log(`${userCount}/${maxUserCount} users connected`)
 
-    // Attributes and sends a turn to the player connected
+		// Attributes and sends a turn to the player connected
 		if (userCount === 1) {
 			socket.emit(Messages.SEND_PLAYER_TURN, 0)
 		} else {
@@ -57,23 +57,23 @@ app.prepare().then(() => {
 
 		// Logic for handling player action
 		socket.on(Messages.PLAYER_ACTION, (gameState, playerTurn, columnIndex) => {
-			console.log(`Player ${playerTurn+1} and tries to play on column ${columnIndex}`);
-			console.log(`Actual game turn: ${gameState.turn+1}`);
+			console.log(`Player ${playerTurn + 1} and tries to play on column ${columnIndex}`);
+			console.log(`Actual game turn: ${gameState.turn + 1}`);
 
-      // checks if the turn of the player that sends an action accords to the gameState turn
+			// checks if the turn of the player that sends an action accords to the gameState turn
 			if (gameState.turn === playerTurn) {
-        // sends the player action to all the players to update the state
+				// sends the player action to all the players to update the state
 				io.emit(Messages.UPDATE_GAME_STATE, columnIndex);
-        console.log(`Player ${playerTurn+1} can play`);
+				console.log(`Player ${playerTurn + 1} can play`);
 			} else {
-        console.log(`Player ${playerTurn+1} cannot play`);
-      }
+				console.log(`Player ${playerTurn + 1} cannot play`);
+			}
 		})
 
-    // disconnects all users when a user disconnects of the room
+		// disconnects all users when a user disconnects of the room
 		socket.on("disconnect", () => {
-      userCount = 0
-      disconnectAllSockets()
+			userCount = 0
+			disconnectAllSockets()
 		});
 	})
 
